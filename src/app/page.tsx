4cad8af332be0d0da578c61932974d34d909bfeb
Ethan <et.l.nicholas@gmail.com>
2023-02-15 21:34:54 +1100

    decide background color and upload personal photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import "./globals.css"
+import personalImage from "../Resouce/Personal-photo-1-transformed.png"
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold text-red-500">
-      planning the layout of portfolio.
-      </h1>
+    <main >
+      <h1 className="text-3xl font-bold text-white"> Ethan </h1>
+      <div className='flex justify-center'>
+        <Image src={personalImage} alt="my photo" width={300}/>
+      </div>
+
     </main>
   )
 }

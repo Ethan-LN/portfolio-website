6eedfb2ab50df7c0dd192b91613f75408610335b
Ethan <et.l.nicholas@gmail.com>
2023-02-20 18:36:15 +1100

    add hover effect to the light mode

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,15 +3,36 @@ import React from "react";
 import logoWhite from "../Resouce/en-logo-no-bg-white.png";
 import Image from "next/image";
 import { LightMode } from "@mui/icons-material";
+import ReactDOM from "react-dom";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function header({}: Props) {
   return (
+    // header div
     <div className="flex justify-between p-5">
-      <Image src={logoWhite} alt="my logo" className="w-[100px]" />
-      <div className="pt-5">
-        <LightMode sx={{ color: "#fafafa" }} fontSize="large"/>
+      <div className="flex">
+        {/* left header div */}
+        <Image src={logoWhite} alt="my logo" className="w-[90px]" />
+        <div>
+          <SocialIcon
+            url="https://github.com/Ethan-LN"
+            bgColor="transparent"
+            fgColor="gray"
+            className="mt-5 ease-in-out duration-300 hover:mt-0"
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/ethan-lang-nicholas/"
+            bgColor="transparent"
+            fgColor="gray"
+            className="mt-5 ease-in-out duration-300 hover:mt-0"
+          />
+        </div>
+      </div>
+      <div className="pt-7">
+        {/* right header div  */}
+        <LightMode sx={{ color: "#fafafa", fontSize: 30 }} className="text-md hover:text-[35px] hover:text-red-400" />
       </div>
     </div>
   );

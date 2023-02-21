d6ea3921b7c1a6266ebf785336da6553a4437775
Ethan <et.l.nicholas@gmail.com>
2023-02-21 14:54:07 +1100

    add google fonts to tailwind, adjust the layout of design, add motion framer typerscript, add cursor effect

--- src/Components/BackgroundCircles.tsx
+++ src/Components/BackgroundCircles.tsx
@@ -1,24 +1,20 @@
 import React from "react";
 import imageCartoonised from "../Resouce/personal-catoonized-no-bg.png";
 import Image from "next/image";
+import Intro from "./Intro";
 
 type Props = {};
 
 export default function BackgroundCircles({}: Props) {
   return (
     <div>
-      <div className="h-[800px] relative flex justify-center items-center">
-        <div className="absolute h-[760px] w-[760px] rounded-full border-4 border-r-transparent border-fuchsia-300 align-[-0.125em] text-primary animate-[spin_30s_linear_infinite]" />
-        <div className="absolute h-[620px] w-[620px] rounded-full border-4 border-r-transparent border-sky-300 align-[-0.125em] text-primary animate-[spin_20s_linear_infinite]" />
-        <div className="absolute h-[460px] w-[460px] rounded-full border-4 border-r-transparent border-gray-500 align-[-0.125em] text-primary animate-[spin_25s_linear_infinite]" />
-        <div className="absolute h-[280px] w-[280px] rounded-full border-4 border-r-transparent border-fuchsia-200 align-[-0.125em] text-primary animate-[spin_30s_linear_infinite]" />
-        <Image
-        src={imageCartoonised}
-        alt="my photo"
-        className="w-40 h-40 bg-pink-200 rounded-full object-cover items-center"
-      />
+      <div className="h-[800px] relative flex justify-center items-center space-y-8">
+        <div className="absolute h-[760px] w-[760px] rounded-full border-2 border-r-transparent border-fuchsia-300 align-[-0.125em] text-primary animate-[spin_30s_linear_infinite] z-8" />
+        <div className="absolute h-[620px] w-[620px] rounded-full border-2 border-r-transparent border-sky-300 align-[-0.125em] text-primary animate-[spin_20s_linear_infinite] z-8" />
+        <div className="absolute h-[460px] w-[460px] rounded-full border-2 border-r-transparent border-gray-500 align-[-0.125em] text-primary animate-[spin_25s_linear_infinite] z-8" />
+        <div className="absolute h-[280px] w-[280px] rounded-full border-2 border-r-transparent border-fuchsia-200 align-[-0.125em] text-primary animate-[spin_30s_linear_infinite] z-8" />
+        <Intro />
       </div>
-
     </div>
   );
 }
--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,15 +1,47 @@
+"use client"
 import React from "react";
-import BackgroundCircle from "./BackgroundCircle";
-import BackgroundCircles from "./BackgroundCircles";
 import imageCartoonised from "../Resouce/personal-catoonized-no-bg.png";
 import Image from "next/image";
+import { Cursor } from "react-simple-typewriter";
+import { useTypewriter } from "react-simple-typewriter";
 
 type Props = {};
 
 export default function Intro({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "✨ Hi, My Name Is Ethan Lang Nicholas 💻",
+      "✨ GuyWhoLoveRunningAndHiking.jsx ",
+      "✨ But_Loves_To_Codes_More.py",
+    ],
+    loop:true,
+    delaySpeed:2000,
+  });
+
   return (
-    <div className="flex flex-col items-center justify-center text-center overflow-hidden ">
-      <BackgroundCircles />
+    <div className="flex flex-col justify-center items-center text-center ">
+      <Image
+        src={imageCartoonised}
+        alt="my photo"
+        className="w-40 h-40 bg-pink-100 rounded-full object-cover items-center mt-[150px]"
+      />
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px] mt-[30px]">
+          Software.Developer
+        </h2>
+        <div className="relative mt-[80px]">
+          <div className="relative flex items-center justify-center">
+            <h1 className="absolute flex flex-1 text-center text-white text-xl font-roboto bg-slate-900 border-rounded-md z-10">
+            <span >{text}
+            </span>
+            <Cursor cursorColor="#f0abfc" />
+            </h1>
+            <h1 className="absolute text-center text-gray-500 text-8xl font-bodoni uppercase z-9">
+              E L N
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

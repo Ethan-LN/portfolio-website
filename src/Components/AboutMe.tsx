d38288a67b1a9955675a1a119e5634cec82f46c4
Ethan <et.l.nicholas@gmail.com>
2023-02-26 22:14:18 +1100

    fix the display bug of scrolling change the head image

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -19,7 +19,7 @@ function AboutMe({}: Props) {
             viewport={{ once: true }}
             src="https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fbumblebee.png?alt=media&token=163fd0e3-ddfd-493f-b2ab-6fe63c7da56c"
             alt="my gihub photo"
-            className=" w-60 h-60 bg-pink-200 rounded-full object-contain md:mt-40 md:rounded-md"
+            className=" w-60 h-60 bg-pink-200 rounded-full object-contain md:mt-40 md:rounded-md items-center"
           />
         </div>
       </div>

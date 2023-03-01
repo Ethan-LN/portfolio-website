0c829c44e1a3d01b50fd58ce6976c7c7900e63f6
Ethan <et.l.nicholas@gmail.com>
2023-03-01 12:06:07 +1100

    add buttons link to github and hosted website, add example of tack stack

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -11,30 +11,54 @@ function Projects({}: Props) {
       <h3 className="container absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl text-center">
         projects
       </h3>
-      {/* <h1 className="text-white">test-86</h1> */}
+      <h1 className="text-white">test-02</h1>
       {ApiProjects.map((project, index) => {
         return (
-          <div key={index} className="snap-start">
+          <div key={index} className="snap-center">
             <div className="flex gap-10 flex-col md:flex-row justify-center">
               <div className="flex items-center flex-col gap-10 min-w-fit md:ml-20 mt-28">
                 <h1 className="text-xl font-bold text-gray-500 mt-20 text-center">
                   {project.title}
                 </h1>
-                <div className="flex flex-col gap-10">
+                <div className="flex flex-col gap-3">
                   <img
                     src={project.src}
                     alt={project.alt}
-                    id={index.toString()}
                     className=" relative w-[100%] rounded-md object-contain max-w-md "
                   />
+                  <div className="flex justify-around mx-70%">
+                    <a href={project.link.github}>
+                      <img
+                        alt="github link"
+                        src={project.linkImg.github}
+                        className="w-28 h-8 rounded-md object-contain p-1 bg-gray-700  hover: hover:bg-slate-500 cursor-pointer"
+                      />
+                    </a>
+                    <a href={project.link.website}>
+                      <img
+                        alt="website link"
+                        src={project.linkImg.website}
+                        className="w-28 h-8 rounded-md p-1 object-contain bg-gray-700  hover:bg-slate-500 cursor-pointer "
+                      />
+                    </a>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center flex-col md:mt-40 max-w-[700px]">
-                <h2 className="text-xl font-bold text-gray-500 mt-20 ">
-                  Highlight
-                </h2>
+              <div className="flex items-center flex-col md:mt-[192px] max-w-[700px]">
+                <h2 className="text-xl font-bold text-gray-500 ">Highlight</h2>
                 <p className="text-gray-400 p-10 ">{project.desc}</p>
                 <div className="border border-fuchsia-300 w-[80%]" />
+                <div className="flex flex-start gap-5 mt-10">
+                  {project.stack.map((tool, index) => {
+                    return (
+                      <img
+                        key={index}
+                        src={tool.url}
+                        className="w-12 h-auto rounded-full p-1"
+                      />
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </div>
--- src/pages/api/ApiProjects.tsx
+++ src/pages/api/ApiProjects.tsx
@@ -1,45 +1,65 @@
 export const ApiProjects = [
   {
     src: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Finstagram-clone.png?alt=media&token=da4d7bb3-dfbf-4430-b269-7fb9b8d4038d",
-    alt:"instagram-clone",
+    alt: "instagram-clone",
     title: "Instagram Clone and Firebase",
     desc: "This is an Instagram clone that utilizes Firebase authentication and storage, enabling users to register, log in, create posts, and leave comments on posts.",
     stack: [
-        {
-          url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FReact.png?alt=media&token=4e01c355-e931-4b40-a7fb-8c215972ea35",
-        },
-  
-        {
-          url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
-        },
-  
-        {
-          url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
-        },
-      ],
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FReact.png?alt=media&token=4e01c355-e931-4b40-a7fb-8c215972ea35",
+      },
+
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
+      },
+
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
+      },
+    ],
+    linkImg: {
+      github:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fexternal-links%2FGitHub-long-logo.png?alt=media&token=ada0c3ca-6f0c-40ba-92db-ebfd4b9193fa",
+      website:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fexternal-links%2Fwebsite-link-icon.png?alt=media&token=07bf6d1f-68f3-47fd-b127-a933d8794817",
+    },
+    link: {
+      github: "https://github.com/Ethan-LN/react-clone-instagram",
+      website: "https://instagram-clone-42aea.web.app/",
+    },
   },
   {
     src: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FE-commerce.png?alt=media&token=a11aea67-3391-4265-ad68-125102453978",
-    alt:"e-commerce",
+    alt: "e-commerce",
     title: "E-commerce website front-end",
     desc: "I created this front-end of an e-commerce website using Tailwind CSS and Material UI icons, and made multiple pages with React Hook and React Router.",
     stack: [
-        {
-          url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FReact.png?alt=media&token=4e01c355-e931-4b40-a7fb-8c215972ea35",
-        },
-  
-        {
-          url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
-        },
-  
-        {
-          url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
-        },
-      ],
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FReact.png?alt=media&token=4e01c355-e931-4b40-a7fb-8c215972ea35",
+      },
+
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
+      },
+
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
+      },
+    ],
+    linkImg: {
+      github:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fexternal-links%2FGitHub-long-logo.png?alt=media&token=ada0c3ca-6f0c-40ba-92db-ebfd4b9193fa",
+      website:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fexternal-links%2Fwebsite-link-icon.png?alt=media&token=07bf6d1f-68f3-47fd-b127-a933d8794817",
+    },
+    link: {
+      github: "https://github.com/Ethan-LN/e-commerce-website-v2",
+      website: "https://ethan-ln.github.io/e-commerce-website-v2/",
+    },
   },
   {
     src: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fto-do-list.png?alt=media&token=18393e76-05fe-4e73-ad7f-4abb9f777cc2",
-    alt:"Todo-list",
+    alt: "Todo-list",
     title: "To-Do List",
     desc: "              This web app is a to-do app equipped with dynamic form validation. Additionally, it is using an API that fetches relevant images based on word tag, which are then used as the hero image for the corresponding task card.",
     stack: [
@@ -55,5 +75,15 @@ export const ApiProjects = [
         url: "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2FTypeScript.png?alt=media&token=1ef4397f-f8e2-4a0b-bb59-9af8e2351dba",
       },
     ],
+    linkImg: {
+      github:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fexternal-links%2FGitHub-long-logo.png?alt=media&token=ada0c3ca-6f0c-40ba-92db-ebfd4b9193fa",
+      website:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-f533e.appspot.com/o/profileImgae%2Fexternal-links%2Fwebsite-link-icon.png?alt=media&token=07bf6d1f-68f3-47fd-b127-a933d8794817",
+    },
+    link: {
+      github: "https://github.com/Ethan-LN/to-do-list-v2",
+      website: "https://ethan-ln.github.io/to-do-list-v2/",
+    },
   },
 ];

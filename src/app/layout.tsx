42ea033acd59c117f46e5825fe130a31d1e239ef
Ethan <et.l.nicholas@gmail.com>
2023-03-03 21:01:15 +1100

    hot fix and clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     // <html lang="en" className="dark">
     <html
       lang="en"
-      className="dark snap-y snap-mandatory overflow-scroll scroll-smooth z-0 m-0 p-0 scrollbar-thumb-fuchsia-300 scrollbar-track-gray-700 dark:scrollbar-track-neutral-300 scrollbar-corner-fuchsia-400 scrollbar-rounded-md scrollbar"
+      className="dark snap-y snap-mandatory overflow-scroll scroll-smooth z-0 m-0 p-0 scrollbar-thumb-fuchsia-300 scrollbar-track-neutral-200 dark:scrollbar-track-gray-700 scrollbar-corner-fuchsia-400 scrollbar-rounded-md scrollbar"
     >
       {/*
         <head /> will contain the components returned by the nearest parent

6f4cb1b38fc1d113702d3b91855238ab8d544840
Ethan <et.l.nicholas@gmail.com>
2023-03-03 18:16:23 +1100

    clean and formatting the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,31 @@
 import "./globals.css";
-import Header from "@/Components/Header";
 import BackgroundCircles from "@/Components/BackgroundCircles";
 import AboutMe from "@/Components/AboutMe";
 import Skills from "@/Components/Skills";
 import Projects from "@/Components/Projects";
 import Footer from "@/Components/Footer";
 import Contact from "@/Components/Contact";
-// import { ThemeProvider } from "next-themes";
-// import 'react';
-// import { useState } from "react";
 
 export default function Home() {
-  // const [bgColor, setBgColor] = useState("bg-slate-900")
-  // const switchMode = () => { setBgColor("bg-neutral-200")}
   return (
-    // <ThemeProvider enableSystem={true} attribute="darkMode">
-      <main >
-        <Header />
-        <section id="hero" className="snap-start">
-          <BackgroundCircles />
-        </section>
-        <section id="about" className="snap-center">
-          <AboutMe />
-        </section>
-        <section id="skills" className="snap-center">
-          <Skills />
-        </section>
-        <section id="projects" className="snap-none">
-          <Projects />
-        </section>
-        <section>
-          <Contact />
-        </section>
-        <Footer />
-      </main>
+    <main>
+      <section id="hero" className="snap-start">
+        <BackgroundCircles />
+      </section>
+      <section id="about" className="snap-center">
+        <AboutMe />
+      </section>
+      <section id="skills" className="snap-center">
+        <Skills />
+      </section>
+      <section id="projects" className="snap-none">
+        <Projects />
+      </section>
+      <section>
+        <Contact />
+      </section>
+      <Footer />
+    </main>
     // </ThemeProvider>
   );
 }
